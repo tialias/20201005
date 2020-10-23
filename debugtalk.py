dc8f5eac0b67eed7be77d795148c489e840c51ff
liangliang tian <liangliang.tian@jinshuju.net>
2020-10-23 14:00:07 +0800

    add all fields test

--- debugtalk.py
+++ debugtalk.py
@@ -1,3 +1,6 @@
+import base64
+import os
+import sys
 import time, uuid
 
 from httprunner import __version__
@@ -17,3 +20,11 @@ def sleep(n_secs):
 
 def gen_nodeId():
     return str(uuid.uuid4())
+
+
+def get_bs64_image():
+    with open(sys.path[0]+"/data/attachment.jpg", "rb") as f:
+        base64_data = base64.b64encode(f.read())
+        return base64_data
+
+
